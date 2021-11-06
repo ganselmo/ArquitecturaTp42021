055f5aee456df60ba2ea85ca3a3507fe4d0f047e
Lucianosc <scaminaci.luciano@gmail.com>
2021-11-06 17:49:00 -0300

    agregados Producto controller y service

---
+++ src/main/java/tp4/despensa/controller/ProductoController.java
@@ -0,0 +1,66 @@
+package tp4.despensa.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tp4.despensa.entities.Producto;
+import tp4.despensa.services.ProductoService;
+
+@RestController
+@RequestMapping("/producto")
+public class ProductoController {
+	private static Logger LOG = LoggerFactory.getLogger(ProductoController.class);
+
+	@Autowired
+	private ProductoService productoService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Producto> getProducto(@PathVariable("id")int id){
+		LOG.info("Buscando producto {}", id);
+		Optional<Producto> producto = this.productoService.getProducto(id);
+		
+		if (producto.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}else {
+			return new ResponseEntity<Producto>(producto.get(), HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<?> addProducto(@RequestBody Producto p){
+		boolean ok = this.productoService.addProducto(p);
+		if(!ok) {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}else {
+			return new ResponseEntity<Producto>(p, HttpStatus.OK);
+		}
+	}
+	
+	@DeleteMapping("")
+	public ResponseEntity<?> deleteProducto(@RequestBody Producto p){
+		boolean ok = this.productoService.deleteProducto(p);
+		if(!ok) {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}else {
+			return new ResponseEntity<Producto>(p, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/all")
+	public List<Producto> getAll(){
+		return this.productoService.getProductos();
+	}
+}
---
+++ src/main/java/tp4/despensa/services/ProductoService.java
@@ -0,0 +1,47 @@
+package tp4.despensa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tp4.despensa.entities.Producto;
+import tp4.despensa.repositories.ProductoRepository;
+
+
+@Service
+public class ProductoService {
+
+	@Autowired
+	private ProductoRepository productos;
+	
+	public Optional<Producto> getProducto(int id) {
+		return this.productos.findById(id);
+	}
+	
+	@Transactional
+	public Boolean addProducto(Producto p) {
+		this.productos.save(p);
+		if(this.getProducto(p.getId()) != null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	@Transactional
+	public Boolean deleteProducto(Producto p) {
+		this.productos.delete(p);
+		if(this.getProducto(p.getId()) == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public List<Producto> getProductos() {
+		return this.productos.findAll();
+	}
+}
